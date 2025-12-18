56beb8dd0bd38a8d0527f115052eaa6eedb3cc79
Abdelrhman Alzyoud <jesixpc@outlook.com>
2025-12-18 16:17:21 +0300

    std

---
+++ students_success_tracker/main.py
@@ -0,0 +1,12 @@
+import sys
+import sqlite3
+
+if sys.prefix == sys.base_prefix:
+    raise RuntimeError(
+        "Virtual environment not active. "
+        "Activate it with: source .venv/bin/activate"
+    )
+
+
+
+
